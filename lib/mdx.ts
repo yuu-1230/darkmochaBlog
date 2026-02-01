d86cb683c4fd1bb28f3204b86b258d656e4456ba
darkmocha <yuto.nagata1230@gmail.com>
2026-02-01 18:18:56 +0900

    feat: add getAllPosts to load and sort posts by date

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -8,17 +8,37 @@ type Frontmatter = {
   tags?: string[];
 };
 type PostData = {
+  slug: string;
   content: string;
   frontmatter: Frontmatter;
 };
+const postsDirectory = path.join(process.cwd(), "content/posts");
 //ブログを表示するために、サーバーの中にあるファイルを読み込んで、プログラムで扱えるデータに変換する
 export const getPost = (slug: string): PostData => {
-  const postsDirectry = path.join(process.cwd(), "content/posts");
-  const fullPath = path.join(postsDirectry, slug + ".mdx");
+  const fullPath = path.join(postsDirectory, slug + ".mdx");
   const fileContents = fs.readFileSync(fullPath, "utf-8");
   const matterResult = matter(fileContents);
   return {
+    slug,
     content: matterResult.content,
     frontmatter: matterResult.data as Frontmatter,
   };
+};
+
+export const getAllPosts = (): PostData[] => {
+  const files = fs.readdirSync(postsDirectory);
+  //拡張子削除
+  const posts = files.map((fileName) => {
+    const slug = path.parse(fileName).name;
+    return getPost(slug);
+  });
+
+  // 日付が新しい順に並び替え (降順)
+  return posts.sort((a, b) => {
+    if (a.frontmatter.date < b.frontmatter.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
 };
